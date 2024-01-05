c2368992ab95b0d41d8c708dddc67d33a25ac053
Luaxwing <nick890111@gmail.com>
2024-01-05 11:11:56 +0800

    20240105

--- api/db.php
+++ api/db.php
@@ -1,171 +1,119 @@
-<?php
+<?php 
 date_default_timezone_set("Asia/Taipei");
-// session_save_path("/tmp");
 session_start();
-class DB
-{
-    // 定義class函數 "資料庫" "PDO" "TABLE"
+class DB{
+
     protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db15";
     protected $pdo;
     protected $table;
-    //$sql 放置MariaDB 的查找語句
-// 
-// 執行DB時 將自訂義的table 帶入 DB函數
-// PDO:PHP Data Objects_登入
+    
     public function __construct($table)
     {
-        $this->table = $table;
-        $this->pdo = new PDO($this->dsn, 'root', '');
-    }
-    // 
-// 測試檢視資料表用
-    function all($where = '', $other = '')
-    {
-        $sql = "select * from `$this->table` ";
-        $sql = $this->sql_all($sql, $where, $other);
-        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        $this->table=$table;
+        //$this->pdo=new PDO($this->dsn,'s1120401','s1120401');
+        $this->pdo=new PDO($this->dsn,'root','');
     }
 
-    function count($where = '', $other = '')
+
+    function all( $where = '', $other = '')
     {
+        $sql = "select * from `$this->table` ";
+        $sql =$this->sql_all($sql,$where,$other);
+        return  $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function count( $where = '', $other = ''){
         $sql = "select count(*) from `$this->table` ";
-        $sql = $this->sql_all($sql, $where, $other);
+        $sql=$this->sql_all($sql,$where,$other);
+        return  $this->pdo->query($sql)->fetchColumn(); 
+    }
+    private function math($math,$col,$array='',$other=''){
+        $sql="select $math(`$col`)  from `$this->table` ";
+        $sql=$this->sql_all($sql,$array,$other);
         return $this->pdo->query($sql)->fetchColumn();
     }
-
-
-    function math($math, $col = "*", $array = "", $other = "")
-    {
-        $sql = "select $math(`$col`) from `$this->table` ";
-        $sql = $this->sql_all($sql, $array, $other);
-        return $this->pdo->query($sql)->fetchColumn();
+    function sum($col='', $where = '', $other = ''){
+        return  $this->math('sum',$col,$where,$other);
     }
-
-
-
-    // SUM-計算table v col 中的 "數值"
-// 要指定欄位
-    function sum($col, $where = '', $other = '')
-    {
-        $sql = "select sum(`$col`) from `$this->table` ";
-        $sql = $this->sql_all($sql, $where, $other);
-        return $this->pdo->query($sql)->fetchColumn();
-    }
-    // 
-
-    // max
-// 要指定欄位
-    function max($col, $where = '', $other = '')
-    {
-        $sql = "select max(`$col`) from `$this->table` ";
-        $sql = $this->sql_all($sql, $where, $other);
-        return $this->pdo->query($sql)->fetchColumn();
-        ;
-    }
-    // 
-
-    // min
-// 要指定欄位
-    function min($col, $where = '', $other = '')
-    {
-        $sql = "select min(`$col`) from `$this->table` ";
-        $sql = $this->sql_all($sql, $where, $other);
-        return $this->pdo->query($sql)->fetchColumn();
-        ;
-    }
-    // 
-
-    // 
-    // 查找特定ID 或 條件
-    function find($a)
+    function max($col, $where = '', $other = ''){
+        return  $this->math('max',$col,$where,$other);
+    }  
+    function min($col, $where = '', $other = ''){
+        return  $this->math('min',$col,$where,$other);
+    }  
+    
+    function find($id)
     {
         $sql = "select * from `$this->table` ";
-
-        if (is_array($a)) {
-            $tmp = $this->a2s($a);
+    
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
             $sql .= " where " . join(" && ", $tmp);
-        } else if (is_numeric($a)) {
-            $sql .= " where `id`='$a'";
-        } else {
-            echo "錯誤:參數的資料型態比須是數字或陣列";
-        }
+        } else if (is_numeric($id)) {
+            $sql .= " where `id`='$id'";
+        } 
         //echo 'find=>'.$sql;
         $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
         return $row;
     }
-    // 
-    // 做儲存/更新用
-    function save($array)
-    {
-        // 驗證資料是否存在
-        // 若存在則更新內容
-        if (isset($array['id'])) {
+    
+    function save($array){
+        if(isset($array['id'])){
             $sql = "update `$this->table` set ";
-
+    
             if (!empty($array)) {
                 $tmp = $this->a2s($array);
-            } else {
-                echo "錯誤:缺少要編輯的欄位陣列";
-            }
-
+            } 
+        
             $sql .= join(",", $tmp);
             $sql .= " where `id`='{$array['id']}'";
-        }
-        // 
-        // 若不存在則新增一筆資料
-        else {
+        }else{
             $sql = "insert into `$this->table` ";
             $cols = "(`" . join("`,`", array_keys($array)) . "`)";
             $vals = "('" . join("','", $array) . "')";
-
+        
             $sql = $sql . $cols . " values " . $vals;
         }
 
         return $this->pdo->exec($sql);
     }
-    // 
-// 刪除資料
+
     function del($id)
     {
         $sql = "delete from `$this->table` where ";
-
+    
         if (is_array($id)) {
             $tmp = $this->a2s($id);
             $sql .= join(" && ", $tmp);
         } else if (is_numeric($id)) {
             $sql .= " `id`='$id'";
-        } else {
-            echo "錯誤:參數的資料型態比須是數字或陣列";
-        }
+        } 
         //echo $sql;
-
+    
         return $this->pdo->exec($sql);
     }
-    // 
-    // 受不了重複的程式碼
-    // 其他
-    function q($sql)
-    {
+    
+    /**
+     * 可輸入各式SQL語法字串並直接執行
+     */
+    function q($sql){
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
     }
-    // 簡化上述重複程式，詳細見上列函式
-// array to sql
-    private function a2s($array)
-    {
+
+    private function a2s($array){
         foreach ($array as $col => $value) {
             $tmp[] = "`$col`='$value'";
         }
         return $tmp;
     }
-    //  
-    // 
-    private function sql_all($sql, $array, $other)
-    {
+
+    private function sql_all($sql,$array,$other){
+
         if (isset($this->table) && !empty($this->table)) {
-
+    
             if (is_array($array)) {
-
+    
                 if (!empty($array)) {
                     $tmp = $this->a2s($array);
                     $sql .= " where " . join(" && ", $tmp);
@@ -173,13 +121,14 @@ class DB
             } else {
                 $sql .= " $array";
             }
-
+    
             $sql .= $other;
+            // echo 'all=>'.$sql;
+            // $rows = $this->pdo->query($sql)->fetchColumn();
             return $sql;
-        } else {
-            echo "錯誤:沒有指定的資料表名稱";
-        }
+        } 
     }
+
 }
 
 function dd($array)
@@ -188,57 +137,24 @@ function dd($array)
     print_r($array);
     echo "</pre>";
 }
-
-// 記得:這裡的table是輸入字串，不是欄位
-// 引入字串，字串才去帶`col`
-
-function to($url)
-{
+function to($url){
     header("location:$url");
 }
 
-// 分頁標籤
 
-
-
-$Total = new DB('total');
-$User= new DB('user');
-
-
-
-
-
-
-
+$Total=new DB('total');
+$User=new DB('user');
 
 if(!isset($_SESSION['visited'])){
-    if($Total->count(['date'=>date('Y-m-d')])>0){   
+    if($Total->count(['date'=>date('Y-m-d')])>0){
         $total=$Total->find(['date'=>date('Y-m-d')]);
         $total['total']++;
         $Total->save($total);
     }else{
         $Total->save(['total'=>1,'date'=>date('Y-m-d')]);
     }
-
-    // $_SESSION(['visited']) = 1;
-    // wrong
-
-    $_SESSION['visited'] = 1;
-
+    $_SESSION['visited']=1;
 }
-
-// if (!isset($_SESSION['visited'])) {
-//     if ($Total->count(['date' => date('Y-m-d')]) > 0) {
-//         $total = $Total->find(['date' => date('Y-m-d')]);
-//         $total['total']++;
-//         $Total->save($total);
-//     } else {
-//         $Total->save(['total' => 1, 'date' => date('Y-m-d')]);
-//     }
-
-//     $_SESSION['visited'] = 1;
-
-// }
 
 
 ?>
--- index old.php
+++ index old.php
@@ -36,9 +36,9 @@
 			</div>
 			<div class="hal" id="main">
 				<div>
-					<marquee style="width:80%; display:inline-block;">請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章</marquee>
+					<marquee style="width:78%; display:inline-block;">請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章</marquee>
 
-					<span style="width:16%; display:inline-block;">
+					<span style="width:20%; display:inline-block;">
 					<?php 
 					if(!isset($_SESSION['user'])){
 					?>
@@ -47,13 +47,8 @@
 					}else{
 					?>		
 						歡迎,<?=$_SESSION['user'];?> 
-						<button onclick="location.href='./api/logout.php'">登出</button>
+						<button onclick="location.href='./api/logout.php">登出</button>
 					<?php 
-						if($_SESSION['user']=='admin'){
-							?>
-							<button onclick="location.href='back.php'">管理</button>
-							<?php	
-							}
 					}
 					?>						
 					</span>
@@ -75,7 +70,7 @@
 		<div id="bottom">
 			本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved
 			<br>
-			服務信箱：health@test.labor.gov.tw<img src="./home_files/02B02.jpg" width="45">
+			服務信箱：health@test.labor.gov.tw<img src="./img/02B02.jpg" width="45">
 		</div>
 	</div>
 
